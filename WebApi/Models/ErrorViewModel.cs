b307da52ae70669050a3510c3998d2367aa8956a
Dimitry Corbet <44736220+MitryDim@users.noreply.github.com>
2023-01-16 14:43:47 +0100

    Delete WebApi directory

--- WebApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
