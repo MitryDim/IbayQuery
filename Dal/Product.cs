a1b2a22d01dcd4c9f2910cf6ea6ad4f4ed2c2948
ShyZox <lai2val1@gmail.com>
2023-01-16 15:43:08 +0100

    produit

---
+++ Dal/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Price { get; set; }
+
+        public Boolean Available { get; set; }
+
+        public DateTime Added_hour { get; set; }
+
+
+        public Product() { }
+
+
+
+    }
+}
